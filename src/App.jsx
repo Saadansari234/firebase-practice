717aad8db116d44fec540e49a2603b9a29677e33
saadansari234 <saadansari3212@gmail.com>
2024-06-12 00:50:15 +0530

    my second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './App.css'
-// import Storage from './pages/Storage'
-import Authentication from './pages/Authentication'
+import { app } from './context/Firebase'
+import { getFirestore, collection, addDoc, } from "firebase/firestore"
+
+const FireStore = getFirestore(app)
 
 const App = () => {
+
+  const writeData = async () => {
+    const result = await addDoc(collection(FireStore, "cities"), {
+      name: 'Delhi',
+      pincode: 123,
+      lat: 456,
+      long: 678,
+    })
+
+    console.log("result", result)
+  }
+
   return (
-    <div>
-      {/* <Storage /> */}
-      <div>hello world</div>
-      <Authentication/>
+    <div className='App'>
+      <h1>Firestore Database</h1>
+      <button onClick={writeData}>put Data</button>
     </div>
   )
 }
--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,7 +1,4 @@
 import { initializeApp } from 'firebase/app'
-import { createContext, useContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-import { getDatabase, set, ref } from "firebase/database"
 
 const firebaseConfig = {
     apiKey: "AIzaSyDvvnPXREtxjrVvCQvzxd3T0od7pWPlckw",
@@ -13,41 +10,4 @@ const firebaseConfig = {
     databaseURL: "https://project-c1bed-default-rtdb.firebaseio.com/"
 };
 
-const FirebaseApp = initializeApp(firebaseConfig)
-const FirebaseAuth = getAuth(FirebaseApp)
-const DataBase = getDatabase(FirebaseApp)
-const googleProvider = new GoogleAuthProvider();
-
-const FirebaseContext = createContext(null)
-
-
-export const FirebaseProvider = (props) => {
-
-    // signup with email and password
-    const signupWithEmailAndPassword = (email, password) => {
-        return createUserWithEmailAndPassword(FirebaseAuth, email, password)
-    }
-    // signIn with email and password
-    const SigninWithEmailAndPassword = (email, password) => {
-        return signInWithEmailAndPassword(FirebaseAuth, email, password)
-    }
-    // signup with google
-    const signUpWithGoogle = () => {
-        signInWithPopup(FirebaseAuth, googleProvider)
-    }
-
-    // function for database
-    const putData = (key, data) => {
-        return set(ref(DataBase, key), data);
-    }
-
-
-    return (
-        <FirebaseContext.Provider value={{ signupWithEmailAndPassword, SigninWithEmailAndPassword, putData, signUpWithGoogle }}>
-            {props.children}
-        </FirebaseContext.Provider>
-    )
-}
-
-
-export const useFirebase = () => useContext(FirebaseContext)
+export const app = initializeApp(firebaseConfig)
