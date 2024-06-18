83e030e98a2619285cfd123a5550807647d1dac0
saadansari234 <saadansari3212@gmail.com>
2024-06-18 23:57:18 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css'
 import { app } from './context/Firebase'
-import { getFirestore, collection, addDoc, } from "firebase/firestore"
+import { getFirestore, collection, addDoc, getDoc, doc, query, where, getDocs, updateDoc,  } from "firebase/firestore"
 
-const FireStore = getFirestore(app)
+const firebaseApp = getFirestore(app)
 
 const App = () => {
 
   const writeData = async () => {
-    const result = await addDoc(collection(FireStore, "cities"), {
+    const result = await addDoc(collection(firebaseApp, "cities"), {
       name: 'Delhi',
       pincode: 123,
       lat: 456,
@@ -18,10 +18,42 @@ const App = () => {
     console.log("result", result)
   }
 
+  const makeSubCollection= async()=>{
+    await addDoc(collection(firebaseApp,"cities/NYATQDzyMZFFsL7WAyZs/places"),{
+       name:"this is a place",
+       desc:"my desc",
+       date: Date.now()
+    })
+  }
+
+  const getDocument=async()=>{
+    const ref = doc(firebaseApp,"cities","NYATQDzyMZFFsL7WAyZs");
+    const snap= await getDoc(ref)
+    console.log(snap.data())
+  }
+
+  const getDocByQuery=async()=>{
+    const collectionRef= collection(firebaseApp,"users")
+    const q= query(collectionRef, where("isMale","==",true))
+    const snapshot= await getDocs(q)
+    snapshot.forEach((data)=> console.log(data.data()))
+  }
+
+  const update=async()=>{
+    const docRef = doc(firebaseApp,"cities","NYATQDzyMZFFsL7WAyZs")
+    await updateDoc(docRef,{
+      name:"Mumbai"
+    })
+  }
+
   return (
     <div className='App'>
       <h1>Firestore Database</h1>
-      <button onClick={writeData}>put Data</button>
+      <button onClick={writeData}> put Data</button>
+      <button onClick={makeSubCollection}> put sub data</button>
+      <button onClick={getDocument}>get Document</button>
+      <button onClick={getDocByQuery}>get document by query</button>
+      <button onClick={update}>update Document</button>
     </div>
   )
 }
